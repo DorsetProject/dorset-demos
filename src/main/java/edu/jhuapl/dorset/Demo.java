41de38107cd6298165e7269da6fe131fcb8b1cda
cash <cash.costello@gmail.com>
2016-01-25 16:57:57 -0500

    simple echo web service

--- src/main/java/edu/jhuapl/dorset/Demo.java
+++ src/main/java/edu/jhuapl/dorset/Demo.java
@@ -16,10 +16,32 @@
  */
 package edu.jhuapl.dorset;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
  * Dorset Demo
  *
  */
 public class Demo {
 
+    public static void main( String[] args ) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+        jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", EchoResource.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
 }
+
