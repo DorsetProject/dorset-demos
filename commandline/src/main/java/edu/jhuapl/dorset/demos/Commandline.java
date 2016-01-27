26b682182b9e83a2aa3c6ad6ea880a7cfd360c8c
Bishop <caitlyn.bishop@jhuapl.edu>
2016-01-27 11:45:59 -0500

    Added a basic Dorset command line demo

--- commandline/src/main/java/edu/jhuapl/dorset/demos/Commandline.java
+++ commandline/src/main/java/edu/jhuapl/dorset/demos/Commandline.java
@@ -16,6 +16,50 @@
  */
 package edu.jhuapl.dorset.demos;
 
-public class Commandline {
+import java.util.Properties;
+import java.util.Scanner;
 
+import edu.jhuapl.dorset.Application;
+import edu.jhuapl.dorset.Request;
+import edu.jhuapl.dorset.Response;
+import edu.jhuapl.dorset.agent.AgentRegistry;
+import edu.jhuapl.dorset.routing.FixedAgentRouter;
+import edu.jhuapl.dorset.routing.Router;
+
+
+public class Commandline {
+	
+    public static void main(String[] args) {
+        
+        AgentRegistry registry = new AgentRegistry();
+        registry.register("test", new TestAgent(), new Properties());
+        Router router = new FixedAgentRouter("test");
+        Application app = new Application(registry, router);
+        
+        System.out.println("Welcome to the Dorset command line demo! "
+                + "Ask any question or press 'q' if you would like to end the session.");
+        
+        String input = ""; 
+        Scanner in = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\nWhat would you like to ask?");
+            input = in.nextLine();
+            
+            if (input.equals("q")) {
+                break;
+            }
+
+            Request request = new Request(input);
+            Response response = app.process(request);
+            
+            System.out.println(response.getText());
+           
+        }
+        
+        System.out.println("\nThank you for your questions! Come back soon!");
+        in.close();
+
+    }
+  
 }
