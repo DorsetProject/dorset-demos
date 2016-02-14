1be83dca153937104a92a8027b693d6d45cbc41b
Cash Costello <cash.costello@gmail.com>
2016-02-13 21:44:30 -0500

    can now run the web demo from the command line

---
+++ web/src/main/java/edu/jhuapl/dorset/demos/Runner.java
@@ -0,0 +1,25 @@
+package edu.jhuapl.dorset.demos;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Runner {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8888);
+
+        WebAppContext context = new WebAppContext();
+        context.setServer(server);
+        context.setContextPath("/");
+
+        ProtectionDomain protectionDomain = Runner.class.getProtectionDomain();
+        URL location = protectionDomain.getCodeSource().getLocation();
+        context.setWar(location.toExternalForm());
+
+        server.setHandler(context);
+        server.start();
+        server.join();
+    }
+}
